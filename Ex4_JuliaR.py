d52869fb59d02471129c8c72b112e0de66a1d32e
Julia <support@giub.unibe.ch>
2019-03-15 15:33:30 +0100

    first version ex4
    This is the first version of the exercise 4

---
+++ Ex4_JuliaR.py
@@ -0,0 +1,23 @@
+#438745-FS2019-0: Geodata analysis and modelling
+#Julia Ryser, 12.03.2019
+
+#Exercise 4
+
+#The package numpy needs to be installed (and imported)
+
+import numpy
+
+#function extracts the precipitation data from the file
+def read_pre_list(data):
+    pre_data_arrow = numpy.loadtxt(data, skiprows=1, usecols=[2])
+    return pre_data_arrow
+
+#Main skript calls the function and displays some data
+path = r"C:\Users\Julia\PycharmProjects\Exercise3\precip_data.txt"
+pre_data = read_pre_list(path)
+
+min_pre = pre_data.min()
+max_pre = pre_data.max()
+mean_pre = round(pre_data.mean(), 2)
+
+print("minimal precipitation value: {}\nmaximal precipitation value: {}\nmean precipitation value: {}".format(min_pre, max_pre, mean_pre))
